13fc66b89f4bcbbbda86780803a3d8ae1287de57
Yevhen Zarkhin <yevhen.zarkhin@ctdev.io>
2020-07-23 20:31:00 +0300

    Connect to mongo

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-const express = require('express')
-const app = express()
+const app = require('express')()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+mongoose.connect('mongodb://user:password2020@ds147487.mlab.com:47487/baza-baza-db', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const Schema = mongoose.Schema;
+
+
+const blogSchema = new Schema({
+  title:  String,
+  author: String,
+  body: String,
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now },
+});
+
+var Blog = mongoose.model('Blog', blogSchema);
+
+
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/posts', function (req, res) {
+  Blog.find((err, response) => res.send(response))
+})
+
+app.post('/posts', function (req, res) {
+  const {title, body, author} = req.body
+  Blog.create({title, body, author}, function (err, post) {
+    res.send(post);
+  })
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
